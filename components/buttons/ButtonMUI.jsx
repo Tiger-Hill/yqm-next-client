07934acd6ddeda0cc9675c6e78f8e55e5eaabfcd
aminmallol <aminm-34@hotmail.fr>
2023-10-04 11:24:30 +0100

    Done with the landing page. Looks exactly the same as the figma wireframe

---
+++ components/buttons/ButtonMUI.jsx
@@ -0,0 +1,33 @@
+import Button from "@mui/material/Button";
+
+const ButtonMUI = ({ color, backgroundColor, width, height, borderRadius, text, type, disabled }) => {
+  return (
+    <Button
+      style={disabled ? { cursor: "not-allowed" } : { cursor: "pointer" }}
+      sx={{
+        width: width,
+        height: height,
+        marginTop: "2rem",
+        fontSize: "1.7rem",
+        backgroundColor: `${backgroundColor} !important`,
+        color: `${color} !important`,
+        height: height,
+        borderRadius: borderRadius,
+        position: "relative",
+        zIndex: 500,
+
+        "&:hover": {
+          boxShadow: "0 5px 10px rgba(0,0,0,0.25)",
+          cursor: "not-allowed !important",
+        },
+      }}
+      type={type}
+      disabled={disabled}
+      size="large"
+    >
+      {text}
+    </Button>
+  );
+};
+
+export default ButtonMUI;
